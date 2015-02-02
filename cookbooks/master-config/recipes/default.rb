0b85c5175ecc1071efb63fa6017ce3fa3d29be9a
Petrik van der Velde <petrikvandervelde@gmail.com>
2015-02-03 07:39:44 +1300

    Adding the default recipe

---
+++ cookbooks/master-config/recipes/default.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: master-config
+# Recipe:: default
+#
+# Copyright 2014, Patrick van der Velde
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "git"
+include_recipe "configuration"
+include_recipe "plugins"
