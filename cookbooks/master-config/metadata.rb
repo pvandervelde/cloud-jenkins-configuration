f04f9e58b73b82cf66edf998785edb5e7549e9f6
Petrik van der Velde <petrikvandervelde@gmail.com>
2015-02-06 18:26:42 +1300

    Collecting all the files in a single location so that we can create an archive.

---
+++ cookbooks/master-config/metadata.rb
@@ -0,0 +1,9 @@
+name 'master-config'
+maintainer '${CompanyName} (${CompanyUrl})'
+maintainer_email '${EmailDocumentation}'
+license 'All rights reserved'
+description 'Configures a Jenkins master on a Windows server'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '${VersionSemantic}'
+
+depends 'windows'
