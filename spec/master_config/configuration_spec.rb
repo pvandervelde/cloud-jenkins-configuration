af111b1eb204f6f85fb94a5ccb3ef0d916ce6f5e
Petrik van der Velde <petrikvandervelde@gmail.com>
2015-02-15 09:21:40 +1300

    Adding the serverspec configuration checks for the plugins and the global jenkins config.

--- spec/master_config/configuration_spec.rb
+++ spec/master_config/configuration_spec.rb
@@ -1,20 +1,47 @@
 require File.join(File.dirname(__FILE__), '..', 'spec_helper')
-require 'jenkins_api_client'
-
-describe 'jenkins configuration' do
-  begin
-    response = RestClient.get 'http://localhost:8080/api/json'
-    it 'is active an returns the correct version' do
-      expect(response.headers.empty?).to be(false)
-      expect(response.headers.keys).to include(:x_jenkins)
-      expect(response.headers[:x_jenkins]).to eq('1.595')
-    end
-  rescue
-    it 'fails' do
-      # this always fails because there was an exception of some kind
-      expect(false).to be true
-    end
-  end
-end
 
 # Read the configuration via the API?
+jenkins_config_content = <<-XML
+[user]
+<hudson>
+  <numExecutors>0</numExecutors>
+  <mode>EXCLUSIVE</mode>
+  <useSecurity>true</useSecurity>
+  <authorizationStrategy class="hudson.security.AuthorizationStrategy$Unsecured"/>
+  <securityRealm class="hudson.security.SecurityRealm$None"/>
+  <disableRememberMe>false</disableRememberMe>
+  <projectNamingStrategy class="jenkins.model.ProjectNamingStrategy$DefaultProjectNamingStrategy"/>
+  <workspaceDir>\\\\invalid\\workspace</workspaceDir>
+  <buildsDir>\\\\invalid\\data</buildsDir>
+  <viewsTabBar class="hudson.views.DefaultViewsTabBar"/>
+  <myViewsTabBar class="hudson.views.DefaultMyViewsTabBar"/>
+  <clouds>
+  </clouds>
+  <slaves>
+  </slaves>
+  <quietPeriod>5</quietPeriod>
+  <scmCheckoutRetryCount>0</scmCheckoutRetryCount>
+  <views>
+    <hudson.model.AllView>
+      <owner class="hudson" reference="../../.."/>
+      <name>All</name>
+      <filterExecutors>false</filterExecutors>
+      <filterQueue>false</filterQueue>
+      <properties class="hudson.model.View$PropertyList"/>
+    </hudson.model.AllView>
+  </views>
+  <primaryView>All</primaryView>
+  <slaveAgentPort>0</slaveAgentPort>
+  <label></label>
+  <nodeProperties/>
+  <globalNodeProperties>
+    <hudson.slaves.EnvironmentVariablesNodeProperty>
+    </hudson.slaves.EnvironmentVariablesNodeProperty>
+  </globalNodeProperties>
+</hudson>
+  XML
+
+describe file("#{ci_directory}\\config.xml") do
+  it { should be_file }
+  its(:content) { should start_with(jenkins_config_content) }
+end
--- spec/master_config/plugins_spec.rb
+++ spec/master_config/plugins_spec.rb
@@ -26,3 +26,100 @@ describe 'jenkins plugins' do
 end
 
 # configurations
+jenkins_plugin_emailext_version = plugins['email-ext']
+jenkins_emailext_default_recipient = 'cc:builds@example.com'
+jenkins_emailext_default_replyto = 'builds@example.com'
+emailext_config_content = <<-XML
+[user]
+<?xml version='1.0' encoding='UTF-8'?>
+<hudson.plugins.emailext.ExtendedEmailPublisherDescriptor plugin="email-ext@#{jenkins_plugin_emailext_version}">
+  <useSsl>false</useSsl>
+  <charset>UTF-8</charset>
+  <defaultContentType>text/html</defaultContentType>
+  <defaultSubject>${PROJECT_DISPLAY_NAME} - Build # $BUILD_NUMBER - $BUILD_STATUS!</defaultSubject>
+  <defaultBody> ${SCRIPT, template=&quot;groovy-builds-html.template&quot;}</defaultBody>
+  <defaultPresendScript></defaultPresendScript>
+  <defaultClasspath/>
+  <emergencyReroute></emergencyReroute>
+  <maxAttachmentSize>-1</maxAttachmentSize>
+  <recipientList>#{jenkins_emailext_default_recipient}</recipientList>
+  <defaultReplyTo>#{jenkins_emailext_default_replyto}</defaultReplyTo>
+  <excludedCommitters></excludedCommitters>
+  <overrideGlobalSettings>false</overrideGlobalSettings>
+  <precedenceBulk>false</precedenceBulk>
+  <debugMode>false</debugMode>
+  <enableSecurity>true</enableSecurity>
+  <requireAdminForTemplateTesting>true</requireAdminForTemplateTesting>
+  <enableWatching>false</enableWatching>
+</hudson.plugins.emailext.ExtendedEmailPublisherDescriptor>
+  XML
+
+describe file("#{ci_directory}\\hudson.plugins.emailext.ExtendedEmailPublisher.xml") do
+  it { should be_file }
+  its(:content) { should start_with(emailext_config_content) }
+end
+
+jenkins_plugin_gittool_version = plugins['git-client']
+gittool_config_content = <<-XML
+[user]
+<?xml version='1.0' encoding='UTF-8'?>
+<hudson.plugins.git.GitTool_-DescriptorImpl plugin="git-client@#{jenkins_plugin_gittool_version}">
+  <installations class="hudson.plugins.git.GitTool-array">
+    <hudson.plugins.git.GitTool>
+      <name>Default</name>
+      <home>git.exe</home>
+      <properties/>
+    </hudson.plugins.git.GitTool>
+  </installations>
+</hudson.plugins.git.GitTool_-DescriptorImpl>
+  XML
+
+describe file("#{ci_directory}\\hudson.plugins.git.GitTool.xml") do
+  it { should be_file }
+  its(:content) { should start_with(gittool_config_content) }
+end
+
+jenkins_plugin_msbuild_version = plugins['msbuild']
+msbuild_config_content = <<-XML
+[user]
+<?xml version='1.0' encoding='UTF-8'?>
+<hudson.plugins.msbuild.MsBuildBuilder_-DescriptorImpl plugin="msbuild@#{jenkins_plugin_msbuild_version}">
+  <installations>
+    <hudson.plugins.msbuild.MsBuildInstallation>
+      <name>3.5 (x86)</name>
+      <home>C:\\Windows\\Microsoft.NET\\Framework\\v3.5\\msbuild.exe</home>
+      <properties/>
+    </hudson.plugins.msbuild.MsBuildInstallation>
+    <hudson.plugins.msbuild.MsBuildInstallation>
+      <name>3.5 (x64)</name>
+      <home>C:\\Windows\\Microsoft.NET\\Framework64\\v3.5\\msbuild.exe</home>
+      <properties/>
+    </hudson.plugins.msbuild.MsBuildInstallation>
+    <hudson.plugins.msbuild.MsBuildInstallation>
+      <name>4.0 (x86)</name>
+      <home>C:\\Windows\\Microsoft.NET\\Framework\\v4.0.30319\\msbuild.exe</home>
+      <properties/>
+    </hudson.plugins.msbuild.MsBuildInstallation>
+    <hudson.plugins.msbuild.MsBuildInstallation>
+      <name>4.0 (x64)</name>
+      <home>C:\\Windows\\Microsoft.NET\\Framework64\\v4.0.30319\\msbuild.exe</home>
+      <properties/>
+    </hudson.plugins.msbuild.MsBuildInstallation>
+    <hudson.plugins.msbuild.MsBuildInstallation>
+      <name>12.0 (x86)</name>
+      <home>C:\\Program Files (x86)\\MSBuild\\12.0\\Bin\\msbuild.exe</home>
+      <properties/>
+    </hudson.plugins.msbuild.MsBuildInstallation>
+    <hudson.plugins.msbuild.MsBuildInstallation>
+      <name>12.0 (x64)</name>
+      <home>C:\\Program Files (x86)\\MSBuild\\12.0\\Bin\\amd64\\msbuild.exe</home>
+      <properties/>
+    </hudson.plugins.msbuild.MsBuildInstallation>
+  </installations>
+</hudson.plugins.msbuild.MsBuildBuilder_-DescriptorImpl>
+  XML
+
+describe file("#{ci_directory}\\hudson.plugins.msbuild.MsBuildBuilder.xml") do
+  it { should be_file }
+  its(:content) { should start_with(msbuild_config_content) }
+end
